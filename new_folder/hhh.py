0a2bf1c9d87bc3a46deebbfd512c0178f69fc763
armyashe <armyashe205@gmail.com>
2024-03-21 22:23:25 +0700

    what

---
+++ new_folder/hhh.py
@@ -0,0 +1,22 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+c=a+b
+d=str("chao con cac ban")
+my_array = [1,2,3,4,5]
+print("The sum of the two numbers is: ",c,d)
+print(my_array)
+def my_function():
+  print("Hello from a function")
+
+my_function()
+#day là project mới hả em
+
+#em muốn push lên main lun à ?
+## em tính push hết mấy folder đã tạo lên hết lun, mà mò hoài nó ko lên +)))
+# em muốn push lên nhánh nào ?
+#main - nhánh chỉnh cuar project ok!
+# em làm cho anh coi đi!
+# lệnh push của em đó hả
+# em nhập hoài nó báo lỗi này hoàiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+# đợi nó load đi máy em lâu v 
+# bth nhanh lắm, chắc tại anh bạo quá nên nó sợ á haha
